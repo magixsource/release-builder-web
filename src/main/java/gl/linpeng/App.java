552fb70f95ce9d7573e559434164d6682a02f7cd
magixsource <magixsource.cn@gmail.com>
2016-05-06 16:06:45 +0800

    [m] add some comment in app.java

--- src/main/java/gl/linpeng/App.java
+++ src/main/java/gl/linpeng/App.java
@@ -1,17 +1,31 @@
 package gl.linpeng;
 
 import org.osgl.mvc.annotation.GetAction;
+import org.osgl.mvc.annotation.PostAction;
 
 import act.boot.app.RunApp;
 
 /**
- * Hello world!
+ * 模块定制服务的入口
+ * 
+ * @author linpeng
  *
  */
 public class App {
 
+	/**
+	 * 这里应该构建一个页面，给到所有的MODULES，让用户定义勾选
+	 */
 	@GetAction
 	public void index() {
+	}
+
+	/**
+	 * 这里接收到用户定制的MODULE数据，并调用buildService，响应结果给到前端
+	 */
+	@PostAction
+	public void build() {
+
 	}
 
 	public static void main(String[] args) throws Exception {
