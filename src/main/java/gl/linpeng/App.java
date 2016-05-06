b9fe1214aa9867487dd32bcd783410e4400c6197
magixsource <magixsource.cn@gmail.com>
2016-05-06 16:23:55 +0800

    [M] fake code in app

--- src/main/java/gl/linpeng/App.java
+++ src/main/java/gl/linpeng/App.java
@@ -1,5 +1,12 @@
 package gl.linpeng;
 
+import java.util.List;
+
+import gl.linpeng.tools.builder.model.BuildModel;
+import gl.linpeng.tools.builder.model.LocalStorageBuildModel;
+import gl.linpeng.tools.builder.module.LocalStorageModule;
+import gl.linpeng.tools.builder.service.Builder;
+
 import org.osgl.mvc.annotation.GetAction;
 import org.osgl.mvc.annotation.PostAction;
 
@@ -13,19 +20,25 @@ import act.boot.app.RunApp;
  */
 public class App {
 
+	private Builder buildService;
+
 	/**
 	 * 这里应该构建一个页面，给到所有的MODULES，让用户定义勾选
 	 */
 	@GetAction
 	public void index() {
+		List<LocalStorageModule> modules = buildService
+				.loadLocalStorageModules();
 	}
 
 	/**
 	 * 这里接收到用户定制的MODULE数据，并调用buildService，响应结果给到前端
 	 */
 	@PostAction
-	public void build() {
-
+	public void build(List<LocalStorageModule> modules) {
+		LocalStorageBuildModel model = new LocalStorageBuildModel();
+		model.setModules(modules);
+		buildService.build(model);
 	}
 
 	public static void main(String[] args) throws Exception {
