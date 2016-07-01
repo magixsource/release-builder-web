30cb5bfb929d10349e5bbd6e73d518b6b4c696cb
magixsource <magixsource.cn@gmail.com>
2016-07-01 17:08:10 +0800

    [M] frontend string[] supported

--- src/main/java/gl/linpeng/App.java
+++ src/main/java/gl/linpeng/App.java
@@ -1,16 +1,28 @@
 package gl.linpeng;
 
-import java.util.List;
-
-import gl.linpeng.tools.builder.model.BuildModel;
 import gl.linpeng.tools.builder.model.LocalStorageBuildModel;
 import gl.linpeng.tools.builder.module.LocalStorageModule;
 import gl.linpeng.tools.builder.service.Builder;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
 import org.osgl.mvc.annotation.GetAction;
 import org.osgl.mvc.annotation.PostAction;
+import org.osgl.mvc.result.Result;
 
 import act.boot.app.RunApp;
+import act.view.RenderTemplate;
+
+import com.alibaba.fastjson.JSON;
 
 /**
  * 模块定制服务的入口
@@ -20,24 +32,51 @@ import act.boot.app.RunApp;
  */
 public class App {
 
+	@Inject
 	private Builder buildService;
+
+	private DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
 
 	/**
 	 * 这里应该构建一个页面，给到所有的MODULES，让用户定义勾选
+	 * 
+	 * @throws IOException
 	 */
 	@GetAction
-	public void index() {
-		List<LocalStorageModule> modules = buildService
-				.loadLocalStorageModules();
+	public Result index() throws IOException {
+		String text = org.apache.commons.io.IOUtils.toString(ClassLoader
+				.getSystemResourceAsStream("modules.json"));
+		List<LocalStorageModule> modules = JSON.parseArray(text,
+				LocalStorageModule.class);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("modules", modules);
+		return new RenderTemplate(map);
 	}
 
 	/**
 	 * 这里接收到用户定制的MODULE数据，并调用buildService，响应结果给到前端
+	 * 
+	 * @throws IOException
 	 */
-	@PostAction
-	public void build(List<LocalStorageModule> modules) {
+	@PostAction("/build")
+	public void takeStringArray(String[] checkbox) throws IOException {
+		List<LocalStorageModule> buildModules = new ArrayList<LocalStorageModule>();
+		String text = org.apache.commons.io.IOUtils.toString(ClassLoader
+				.getSystemResourceAsStream("modules.json"));
+
+		List<LocalStorageModule> moduels = JSON.parseArray(text,
+				LocalStorageModule.class);
+		Map<String, LocalStorageModule> map = moduels.stream().collect(
+				Collectors.toMap(LocalStorageModule::getId, (p) -> p));
+
+		for (String modelName : checkbox) {
+			LocalStorageModule module = map.get(modelName);
+			if (null != module) {
+				buildModules.add(module);
+			}
+		}
 		LocalStorageBuildModel model = new LocalStorageBuildModel();
-		model.setModules(modules);
+		model.setModules(buildModules);
 		buildService.build(model);
 	}
 
